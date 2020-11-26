ca38c635124c42d12927eff0ba5e6e7bb5839a62
Tixieej <rixtdieuwke@live.nl>
2020-11-26 10:35:42 +0100

    changed main

--- main.c
+++ main.c
@@ -6,18 +6,20 @@
 /*   By: livlamin <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/29 10:25:42 by livlamin      #+#    #+#                 */
-/*   Updated: 2020/10/23 16:03:08 by livlamin      ########   odam.nl         */
+/*   Updated: 2020/11/26 10:35:26 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
 #include <stdio.h> //
+
 int		main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
 		printf("%s\n", argv[1]);
 	}
+	else
+		printf("please give one argument\n");
 	return (0);
 }
