5b00c8891be2a0e0be1920f12c3f712de373be0a
Tixieej <rixtdieuwke@live.nl>
2020-11-26 16:08:51 +0100

    prompt

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: livlamin <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/29 10:25:42 by livlamin      #+#    #+#                 */
-/*   Updated: 2020/11/26 13:24:06 by rixt          ########   odam.nl         */
+/*   Updated: 2020/11/26 15:55:26 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdlib.h>
+
+void	prompt()
+{
+	write(1, "\033[38;5;105mhallo: \e[0m", 23);
+}
 
 int		main(int argc, char **argv)
 {
@@ -30,10 +35,14 @@ int		main(int argc, char **argv)
 		printf("no arguments needed"); //
 		return (0);
 	}
-	while (result == 1)
+	while (1)
 	{
+		//iets met signals? waarom hier?
+		prompt();
 		result = get_next_line(0, &line);
-		printf("%s\n", line);
+		//tokenizer aanroepen
+		//andere dingen aanroepen
+		printf("%s\n", line); //
 		free(line);
 	}
 	return (0);
