f7bc61ae9fd0084bda853518bb920971485d83a7
lisa vlamings <>
2020-12-21 10:04:30 +0100

    main link added for test

---
+++ main_link/main.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/29 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2020/11/26 15:55:26 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <stdio.h> //
+#include <unistd.h>
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdlib.h>
+
+void	prompt()
+{
+	write(1, "\033[38;5;105mhallo: \e[0m", 23);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*line;
+	int		result;
+
+	result = 1;
+	line = NULL;
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("no arguments needed"); //
+		return (0);
+	}
+	while (1)
+	{
+		//iets met signals? waarom hier?
+		prompt();
+		result = get_next_line(0, &line);
+		//tokenizer aanroepen
+		//andere dingen aanroepen
+		printf("%s\n", line); //
+		free(line);
+	}
+	return (0);
+}
