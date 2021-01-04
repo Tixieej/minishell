b17f470726d80b4a13e28d38f8d99a59095dda4e
lisa vlamings <>
2021-01-04 10:32:35 +0100

    main fork and list

--- LV_tests/master_main/main.c
+++ LV_tests/master_main/main.c
@@ -17,23 +17,37 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "ft_list.h"
+#include <signal.h>
 
 
 // Names of environment variables are case-sensitive and must not contain the character ‘=’. 
 //System-defined environment variables are invariably uppercase. 
 // The values of environment variables can be anything that can be represented as a string.
-//A value must not contain an embedded null character, since this is assumed to terminate the string. 
+//A value must not contain an embedded null character, since this is assumed to terminate the string.
+
+int		send_signal (int pid)
+{
+        int ret;
+
+		ret = kill(pid,SIGKILL);
+		printf("sigkill : %d\n", SIGKILL);
+        printf("ret : %d\n",ret);
+		return(ret);
+}
+
 void	prompt()
 {
 	write(1, "\033[38;5;105mhallo: \e[0m", 23);
 }
 
-int		main(int argc, char **argv, char *envp[])
+int		main(int argc, char **argv)
 {
+	pid_t	ret_value;
 	t_list *list;
 	char	*line;
 	int		result;
-	
+
+	ret_value = 7;	
 	list = NULL;
 	result = 1;
 	line = NULL;
@@ -43,23 +57,43 @@ int		main(int argc, char **argv, char *envp[])
 		printf("no arguments needed"); //
 		return (0);
 	}
-	while (result == 1)
+	ret_value = fork();
+	if (ret_value < 0)
+		printf("creating childprocess had failed\n"); //
+	else if (ret_value == 0)
 	{
-		//iets met signals? waarom hier?
-		prompt();
-		result = get_next_line(0, &line);
-		ft_list_push_back(&list, line);
-		while (result == 1)  // voorbeelde cat > test.txt
+		// ret_value = fork(); //twee keer prompt?
+		while (result == 1)
 		{
+		//iets met signals? waarom hier?
+			prompt();
 			result = get_next_line(0, &line);
 			ft_list_push_back(&list, line);
+			while (result == 1)  // voorbeelde cat > test.txt
+			{
+				result = get_next_line(0, &line);
+				ft_list_push_back(&list, line);
+			}
+			// free(line);
+			// line = NULL;
 		}
-		// free(line);
-		// line = NULL;
+		free(line);
+		line = NULL;
+		while (list)  //prints list
+		{
+			printf("%s\n", (char*)list->data);
+			printf("%p\n", (char*)list->next);
+			list = list->next;
+		}
+		if (send_signal(ret_value) == 0)
+			printf("childprocess is killed\n");
 	}
-	free(line);
-	line = NULL;
-	while (list)
+	else
+	{
+		wait(NULL);
+		printf("parent process\n"); //
+	}
+	while (list)  //prints list
 	{
 		printf("%s\n", (char*)list->data);
 		printf("%p\n", (char*)list->next);
