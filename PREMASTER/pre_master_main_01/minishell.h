04b307016a9009035c44892a877846f5cea28bcb
lisa vlamings <>
2021-01-12 14:11:19 +0100

    PREMASTER TEST

---
+++ PREMASTER/pre_master_main_01/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/29 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2020/10/23 16:03:08 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <sys/wait.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include "get_next_line.h"
+# include <unistd.h> //mogen we die gebruiken? voor write
+# include <stdio.h> // WEGHALEN PRINTF
+
+void            check_type(t_list *list);
+void            start_program(t_list *list);
+int             main(int argc, char **argv);
+
+#endif
