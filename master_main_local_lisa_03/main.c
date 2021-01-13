ea8b6c22fc471c737ee4631debfee38efa0b66ef
lisa vlamings <>
2021-01-13 14:15:50 +0100

    proberen met list

--- master_main_local_lisa_03/main.c
+++ master_main_local_lisa_03/main.c
@@ -22,7 +22,7 @@ int		main(int argc, char **argv)//, char **env)
 {
 	t_list	*list;
 
-	list = ft_create_elem(ft_strdup("start"));
+	// list = ft_create_elem(ft_strdup("start"));
 	list = NULL;
 	(void)argv;
 	if (argc != 1)
