bcbeee12b7d88f0d1b3067ef57822b14c98a8eea
lisa vlamings <>
2021-01-14 13:08:18 +0100

    update

---
+++ master_main_local_lisa_05/check_type.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check_type.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/12/01 15:55:26 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	error(char *str, int ret)
+{
+	printf("%s\n", str);
+	if (ret < 0)
+		exit(-1);
+}
+
+void    check_type(t_list **list, char **env)
+{
+	t_list	*begin;
+
+	begin = *list;
+	while (begin)
+	{
+		if (ft_strncmp((const char *)begin->content, "echo", 4) == 0)
+			printf("cat type\n");
+		if (ft_strncmp((const char *)begin->content, "cd", 2) == 0)
+			printf("cd type\n");
+		if (ft_strncmp((const char *)begin->content, "pwd", 3) == 0)
+			printf("pwd type\n");
+		if (ft_strncmp((const char *)begin->content, "export", 6) == 0)
+			printf("export type\n");
+		if (ft_strncmp((const char *)begin->content, "unset", 5) == 0)
+			printf("unset type\n");
+		if (ft_strncmp((const char *)begin->content, "env", 3) == 0)
+			printf("env type\n");
+		if (ft_strncmp((const char *)begin->content, "exit", 4) == 0)
+			printf("exit type\n");
+		else
+			ft_parse(*list, env, begin);
+		// if (ft_parse = -1)
+		// 	error("invalid programme input", -1);
+		begin = begin->next;
+	}
+	begin = *list;
+}
+
+
