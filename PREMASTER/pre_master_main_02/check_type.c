ac8c292f9715e947dd049ecebcdc87c0ec40fd34
lisa vlamings <>
2021-01-14 11:10:59 +0100

    pre master 02 alles gelinkt

---
+++ PREMASTER/pre_master_main_02/check_type.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check_type.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/12/01 15:55:26 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// void	parser()
+// {
+// 	makes struct;
+// }
+
+void    check_type(t_list **list, char **env)
+{
+	t_list	*begin;
+
+	begin = *list;
+	while (begin)
+	{
+		if (ft_strncmp((const char *)begin->content, "cat", 3) == 0)
+			printf("cat type\n");
+		if (ft_strncmp((const char *)begin->content, "cp", 2) == 0)
+			ft_parse(*list, env);
+		if (ft_strncmp((const char *)begin->content, "echo", 4) == 0)
+			printf("echo type\n");
+		begin = begin->next;
+	}
+	begin = *list;
+}
