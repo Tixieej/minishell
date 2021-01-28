243256665b145182d96e6c60cd8adddb107d9531
Tixieej <rixtdieuwke@live.nl>
2021-01-28 18:05:06 +0100

    premaster 6.1 = 6 + compile errors + norm fixes

---
+++ pre_master_main_06.1/libft/ft_create_elem.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_create_elem.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/08/20 12:23:04 by livlamin      #+#    #+#                 */
+/*   Updated: 2019/08/21 21:46:14 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_create_elem(void *data)
+{
+	t_list *list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->content = data;
+		list->next = NULL;
+	}
+	return (list);
+}
