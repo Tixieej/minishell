a045a7a9e01c438b07d38cc90f2a5eeaf2713efd
Tixieej <rixtdieuwke@live.nl>
2021-02-04 14:53:04 +0100

    twee segfaults eruit

---
+++ PREMASTER/pre_master_main_07.2_R/check_type.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check_type.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/28 17:53:20 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/03 14:28:17 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check_type(char **env, t_command *command) //moet list wel mee?
+{
+	t_command	*cur_struct;
+	int			fd;
+	int			process;
+
+	/* dit is een check en kan later weg */
+	// printf("check_type\n");
+	t_list *begin = command->args;
+	while ((command->args))
+	{
+		printf("\targs: [%s]\n", (command->args)->content);
+		command->args = command->args->next;
+	}
+	command->args = begin;
+	/* einde check */
+
+	cur_struct = command;
+	fd = redirection(cur_struct);
+	process = -1;
+	if (command->pipe_right == 1 || command->pipe_left == 1)
+		process = pipes(cur_struct);
+	if (ft_strncmp((const char *)cur_struct->program, "echo", 4) == 0)
+		echo(command, fd);
+	else if (ft_strncmp((const char *)cur_struct->program, "cd", 2) == 0)
+		cd(command);
+	else if (ft_strncmp((const char *)cur_struct->program, "pwd", 3) == 0)
+		pwd(fd);
+	else if (ft_strncmp((const char *)cur_struct->program, "export", 6) == 0)
+		printf("export type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "unset", 5) == 0)
+		printf("unset type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "env", 3) == 0)
+		printf("env type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "exit", 4) == 0)
+		printf("exit type\n");
+	else
+		external(command, env, fd, process);
+		// if (non_builtin = -1)
+		// 	error("invalid programme input", -1);
+}
---
+++ PREMASTER/pre_master_main_07.2_R/external.c
@@ -0,0 +1,173 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   external.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rdvrie <marvin@codam.nl>                     +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by rixt          #+#    #+#                 */
+/*   Updated: 2021/01/28 18:00:24 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char			**list_to_array(t_list **list)
+{
+	int		count;
+	char	**array;
+	int		i;
+	t_list	*current;
+
+	if (!(*list) || !((*list)->content))// klopt dit?
+	{
+		array = (char **)malloc(sizeof(char *));
+		array[0] = NULL;
+		return (array);
+	}
+	current = *list;
+	count = 0;
+	while (current)// misschien met list lopen maar dan met de kopie begin;
+	{
+		current = current->next; //
+		count++;
+	}
+	array = (char **)malloc(sizeof(char *) * (count + 1));
+	i = 0;
+	current = *list;
+	while (i < count)
+	{
+		array[i] = (char *)current->content;
+		current = current->next;
+		i++;
+	}
+	array[i] = NULL;
+	return (array);
+}
+
+void			ft_exec(char *path, t_command cmd, char **env, pid_t process)
+{// je moet alleen forken als je nog niet eerder bent geforked, dus check of je geen child process bent.
+	//pid_t	process;
+	char	**args;
+	t_list	*arglist;
+
+	arglist = cmd.args;
+	ft_lstadd_front(&arglist, ft_create_elem(path));
+	//in create_elem wordt gemalloct dus we moeten nog checken of dat gelukt is.
+	//dat kunnen we toevoegen in create_elem zelf, of hieronder..
+	args = list_to_array(&arglist);
+	if (process != 0)
+	{
+		printf(" ft_exec: forken hieronder!\n");
+		process = fork();
+	}
+	if (process == 0)
+		execve(path, args, env);
+	else
+		wait(NULL);
+}
+
+static void		with_path(t_command cmd, char **env, pid_t process)
+{
+	struct stat		buffer;
+	char			*path;
+
+	path = cmd.program;
+	if (stat(path, &buffer) == 0)
+		ft_exec(path, cmd, env, process);
+	else
+		printf("no such file or directory: %s\n", cmd.program);
+}
+
+static void		attach_path(t_command cmd, char **env, pid_t process)
+{
+	char		**paths;
+	char		*path;
+	struct stat	buffer;
+	int			i;
+
+	paths = make_path_array(env);// check_env aanroepen? dan is het een string en geen array.
+	i = 0;
+	if (paths == NULL) // dit kan mooier. misschien naar de errorfunctie sluizen?
+	{
+		printf("command not found: %s\n", cmd.program);
+		return ;
+	}
+	while (paths[i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(path, cmd.program);
+		//cmd.args[0] = path;
+		if (stat(path, &buffer) == 0)
+		{
+			ft_exec(path, cmd, env, process);
+			break ;
+		}
+		i++;
+	}
+	if (stat(path, &buffer) != 0)
+		printf("command not found: %s\n", cmd.program);
+}
+
+static int		out_redirect(t_command *cmd, int out_fd)
+{
+	int		stdout_fd;
+
+	stdout_fd = dup(STDOUT_FILENO);
+	if (cmd->out_red)
+	{
+		if (stdout_fd < 0)
+			printf("bouw hier error in!\n");
+		if(dup2(out_fd, STDOUT_FILENO) < 0)
+		{
+			printf("Unable to duplicate file descriptor.");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (stdout_fd);
+}
+
+// static int		in_redirect(t_command *cmd, int in_fd)
+// {
+// 	int		stdin_fd;
+	
+// 	stdin_fd = dup(STDIN_FILENO);
+// 	if (cmd->in_red)
+// 	{
+// 		if (stdin_fd < 0)
+// 			printf("bouw hier error in!\n");
+// 		if (dup2(STDIN_FILENO, in_fd) < 0)
+// 		{
+// 			printf("Unable to duplicate file descriptor.");
+// 			exit(EXIT_FAILURE);
+// 		}
+// 	}
+// 	return (stdin_fd);
+// }
+
+void			external(t_command *cmd, char **env, int fd, int process)
+{
+	int		stdout_fd;
+	//int		stdin_fd;
+
+	stdout_fd = out_redirect(cmd, fd);
+	//stdin_fd = in_redirect(cmd);
+	if (ft_strchr(cmd->program, '/') != 0)
+	{
+		with_path(*cmd, env, process);
+	}
+	else
+	{
+		attach_path(*cmd, env, process);
+	}
+	if (dup2(STDOUT_FILENO, stdout_fd) < 0)// dit alleen uitvoeren als er redirections zijn?
+	{
+		printf("Unable to duplicate file descriptor.");
+		exit(EXIT_FAILURE);
+	}
+	// if (dup2(STDIN_FILENO, stdin_fd) < 0)
+	// {
+	// 	printf("Unable to duplicate file descriptor.");
+	// 	exit(EXIT_FAILURE);
+	// }
+}
