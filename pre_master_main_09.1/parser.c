d406f5d9badd488e67279e00a8d67309684ac78f
Tixieej <rixtdieuwke@live.nl>
2021-02-08 18:40:06 +0100

    parser.c > parser > r.83: pointer being freed was not allocated

---
+++ pre_master_main_09.1/parser.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parser.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/08 13:51:22 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void		add_semicolon(t_list **cur_lst, t_command **cur_struct)
+{
+	(*cur_lst) = (*cur_lst)->next;
+	ft_struct_push_back(&(*cur_struct), (char *)(*cur_lst)->content);
+}
+
+static void		add_pipe(t_list **cur_lst, t_command **cur_struct)
+{
+	(*cur_struct)->pipe_right = 1;
+	(*cur_lst) = (*cur_lst)->next;
+	ft_struct_push_back(&(*cur_struct), (char *)(*cur_lst)->content);
+	cur_struct = &(*cur_struct)->next;
+	(*cur_struct)->pipe_left = 1;
+}
+
+static void		add_redirection(t_list **cur_lst, t_command **cur_struct)
+{
+	char *path;
+
+	path = NULL;
+	if (*(*cur_lst)->content == '>')
+	{
+		ft_list_push_back(&((*cur_struct)->out_red), (*cur_lst)->content);
+		(*cur_lst) = (*cur_lst)->next;
+		ft_list_push_back(&((*cur_struct)->out_red), (*cur_lst)->content);
+	}
+	else if (*(*cur_lst)->content == '<')
+	{
+		ft_list_push_back(&((*cur_struct)->in_red), (*cur_lst)->content);
+		(*cur_lst) = (*cur_lst)->next;
+		if ((*cur_lst))
+			ft_list_push_back(&((*cur_struct)->in_red), (*cur_lst)->content);
+	}
+}
+
+void			parser(t_list **list, char **env, t_command *command)
+{
+	t_list		*cur_lst;
+	t_command	**cur_struct;
+
+	cur_lst = *list;
+	cur_struct = &command;
+	ft_struct_push_back(&command, (char *)cur_lst->content);
+	cur_lst = cur_lst->next;
+	while (cur_lst)
+	{
+		printf("%s\n", cur_lst->content);
+		if (*cur_lst->content == ';')
+		{
+			check_type(env, *cur_struct);
+			add_semicolon(&cur_lst, cur_struct);
+			cur_struct = &(*cur_struct)->next;
+		}
+		else if (*cur_lst->content == '|')
+			add_pipe(&cur_lst, cur_struct);
+		else if (*cur_lst->content == '>' || *cur_lst->content == '<')
+			add_redirection(&cur_lst, cur_struct);
+		else
+			ft_list_push_back(&((*cur_struct)->args), cur_lst->content);
+		cur_lst = cur_lst->next;
+	}
+	cur_lst = *list; // weg?
+	//print_cur_struct(command); // weg !! // zorg dat deze functie de t_lists weer op begin zet!!!
+	check_type(env, *cur_struct);
+	command = ft_clear_linked_struct(command);
+	printf("check of struct is leeg hieronder\n"); // weg
+	print_cur_struct(command); // weg !!
+	// command = NULL; ?
+	//ft_lstclear(&(*list), free); // klopt dit? DEZE GEEFT EEN FOUT!!! pointer being freed was not allocated
+	*list = NULL; //klopt dit?
+	// ft_lstclear(&cur_lst, free);
+	// ft_lstclear(&list, free);  ft_strctclear schrijven
+}
---
+++ pre_master_main_09.1/redirection.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   redirection.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/03 13:16:00 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/08 11:01:30 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//LET OP CLOSEN!!
+
+int    in_red(t_command *command)
+{
+    t_command	**cur_struct;
+
+    cur_struct = &command;
+    while ((*cur_struct)->in_red)
+    {
+        if (!(ft_strncmp((const char *)(*cur_struct)->in_red->content, "<", 1)))
+        {
+            (*cur_struct)->in_red = (*cur_struct)->in_red->next;
+            if ((*cur_struct)->in_red)
+                command->fd_in = open((const char *)(*cur_struct)->in_red->content, O_RDONLY, 0644);
+        }
+        if (command->fd_in < 0)
+            error_handler("open file failed\n", NULL, NULL); //checken
+        (*cur_struct)->in_red = (*cur_struct)->in_red->next;
+    }
+	return (command->fd_in);
+}
+
+int    out_red(t_command *command)
+{
+    t_command	**cur_struct;
+
+    cur_struct = &command;
+    while ((*cur_struct)->out_red)
+    {
+        if (!(ft_strncmp((const char *)(*cur_struct)->out_red->content, ">>", 2)))
+        {
+            (*cur_struct)->out_red = (*cur_struct)->out_red->next;
+            if ((*cur_struct)->out_red)
+                command->fd_out = open((const char *)(*cur_struct)->out_red->content, O_CREAT | O_RDWR | O_APPEND, 0644);
+        }
+        else if (!(ft_strncmp((const char *)(*cur_struct)->out_red->content, ">", 1)))
+        {
+            (*cur_struct)->out_red = (*cur_struct)->out_red->next;
+            if ((*cur_struct)->out_red)
+                command->fd_out = open((const char *)(*cur_struct)->out_red->content, O_CREAT | O_RDWR | O_TRUNC, 0644);
+			printf("\tfile geopend, fd = [%d]\n", command->fd_out);
+        }
+        if (command->fd_out < 0)
+            error_handler("open file failed\n", NULL, (*cur_struct));
+        (*cur_struct)->out_red = (*cur_struct)->out_red->next;
+    }
+	return (command->fd_out);
+}
+
+void    redirection(t_command *command)
+{
+    t_command	**cur_struct;
+
+    cur_struct = &command;
+    command->fd_in = 0; //klopt dit?
+    command->fd_out = 1;
+    command->fd_in = in_red(command);
+    command->fd_out = out_red(command);
+	printf("\tfd_out = [%d]\n", command->fd_out);
+    return; //kan die weg?
+}
