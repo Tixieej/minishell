5c7a372295a06363e7ba083389211109ec881e1c
lisa vlamings <>
2021-02-08 11:04:49 +0100

    versie 08 fd_in en fd_out

---
+++ PREMASTER/pre_master_main_08_L/linked_struct.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   linked_struct.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/03 13:16:45 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// t_command		*ft_clear_linked_struct(char *data)
+// {
+// 	t_command *command;
+
+// 	command = malloc(sizeof(t_command));
+// 	if (command)
+// 	{
+//         command->program = data;
+// 		command->args = NULL;
+//         command->pipe_left = 0;
+//         command->pipe_right = 0;
+// 		command->in_red = NULL;
+// 		command->out_red = NULL;
+// 		command->next = NULL;
+// 	}
+// 	return (command);
+// }
+
+ 
+t_command		*ft_create_linked_struct(char *data)
+{
+	t_command *command;
+
+	command = malloc(sizeof(t_command));
+	if (command)
+	{
+        command->program = data;
+		command->args = NULL;
+        command->pipe_left = 0;
+        command->pipe_right = 0;
+		command->in_red = NULL;
+		command->out_red = NULL;
+		command->next = NULL;
+	}
+	return (command);
+}
+
+void		ft_struct_push_back(t_command **begin_list, char *data)
+{
+	t_command	*temp;
+	temp = *begin_list;
+	if (*begin_list)
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = ft_create_linked_struct(data);
+	}
+	else
+		*begin_list = ft_create_linked_struct(data);
+}
