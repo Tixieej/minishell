41108e1cfdb9502d0a903211f35fdba6f726c9c7
Tixieej <rixtdieuwke@live.nl>
2021-02-09 10:25:34 +0100

    9.1 van beiden samengevoegd

---
+++ PREMASTER/pre_master_main_09.2/linked_struct.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   linked_struct.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/09 10:08:56 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_command		*ft_clear_linked_struct(t_command *command)
+{
+	t_command **cur_struct;
+
+	cur_struct = &command;
+	while(*cur_struct)
+	{
+		if ((*cur_struct)->program)
+		{
+			free((*cur_struct)->program); //fixen
+			(*cur_struct)->program = NULL;
+		}
+		if ((*cur_struct)->args)
+		{
+			ft_lstclear(&(*cur_struct)->args, free);
+			(*cur_struct)->args = NULL;
+		}
+		if ((*cur_struct)->in_red)
+		{
+			ft_lstclear(&(*cur_struct)->in_red, free);
+			(*cur_struct)->in_red = NULL;
+		}
+		if ((*cur_struct)->out_red)
+		{
+			ft_lstclear(&(*cur_struct)->out_red, free);
+			(*cur_struct)->out_red = NULL;
+		}
+		(*cur_struct)->pipe_left = 0;
+        (*cur_struct)->pipe_right = 0;
+		(*cur_struct)->fd_in = 0;
+		(*cur_struct)->fd_out = 0;
+		(*cur_struct)->next = NULL;
+		cur_struct = &(*cur_struct)->next;
+		
+	}
+	return (command);
+}
+
+ 
+t_command		*ft_create_linked_struct(char *data)
+{
+	t_command *command;
+
+	command = malloc(sizeof(t_command));
+	if (command)
+	{
+        command->program = ft_strdup(data);
+		command->args = NULL;
+        command->pipe_left = 0;
+        command->pipe_right = 0;
+		command->fd_in = 0;
+		command->fd_out = 0;
+		command->in_red = NULL;
+		command->out_red = NULL;
+		command->next = NULL;
+	}
+	return (command);
+}
+
+void		ft_struct_push_back(t_command **begin_list, char *data)
+{
+	t_command	*temp;
+	temp = *begin_list;
+	if (*begin_list)
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = ft_create_linked_struct(data);
+	}
+	else
+		*begin_list = ft_create_linked_struct(data);
+}
---
+++ PREMASTER/pre_master_main_09.2/pwd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pwd.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: Invalid date        by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/09 10:20:32 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void        pwd(int fd)
+{
+    char *path;
+
+    path = getcwd(NULL, 0);
+    if (path == NULL)
+        error_handler("pwd failure", NULL, NULL); //veranderen met geheugen;
+    path = ft_strjoin(path, "\n");
+    if (write(fd, path, ft_strlen(path)) < 0)
+        printf("error\n");
+    free(path);
+    path = NULL;
+}
