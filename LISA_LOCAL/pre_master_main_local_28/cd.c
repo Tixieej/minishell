80653ff02dcf89205fe3c81bd5e44a34794a058e
lisa vlamings <>
2021-02-11 14:56:49 +0100

    29 begin cd

---
+++ LISA_LOCAL/pre_master_main_local_28/cd.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cd.c                                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/03 13:12:48 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/02/05 15:07:43 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char		*move_backwards(char *path)
+{
+	int len;
+
+	len = ft_strlen(path);
+	while (path[len] != '/')
+	{
+		len--;
+	}
+	return(ft_substr(path, 0, len));
+}
+
+void        *alter_env(char **env, char *var, char *file)
+{
+    int count;
+	int	len_var;
+
+	len_var = ft_strlen(var);
+    count = 0;
+    while (env[count])
+    {
+        if (!ft_strncmp(env[count], var, len_var))
+		{
+			env[count] = ft_strjoin(env[count], "/");
+			env[count] = ft_strjoin(env[count], file);
+		}   
+        count++;
+    }
+    return (NULL);
+}
+
+void        cd(t_command *command, char **env, char *path)
+{
+	struct stat		buffer;
+
+    path = getcwd(NULL, 0);
+    if (path == NULL)
+        error_handler("pwd failure", NULL, NULL); //veranderen met geheugen;
+	if (command->args == NULL)
+	{
+		path = check_env(env, "HOME=");
+		if (path == NULL)
+			error_handler("HOME= non-existing", NULL, NULL);
+		if (chdir(path) != 0)
+			error_handler("path non-existing", NULL, NULL);
+		return;
+	}
+	if (command->args->content[0] == '.' && command->args->content[1] == '.')
+		path = move_backwards(path);   //LEAK?
+	else if (command->args->content[0] == '.' && command->args->content[1] == '/')
+		command->args->content = ft_substr(command->args->content, 2, (ft_strlen(path) - 2));
+
+	else if (stat(command->args->content, &buffer) == 0)
+	{
+		while (command)
+		{
+			if (stat(command->args->content, &buffer) == 0)
+			{
+				alter_env(&(*env), "PWD=", command->args->content);
+				path = ft_strjoin(path, "/");
+				path = ft_strjoin(path, command->args->content);
+			}
+			command = command->next;
+		}
+	}
+	else 
+		error_handler("file non-existing", NULL, NULL);
+	if (chdir(path) != 0)
+		error_handler("path non-existing", NULL, NULL);
+	// printf("path: %s", path);
+	// printf("PWD 1: %s\n", get_pointer_env(env, "PATH=")); 
+	// printf("PWD: %s\n", getcwd(NULL, 0));
+}
