b19191a14a9c2bc96eb543741845bca1bcfb87cb
lisa vlamings <>
2021-02-26 14:17:05 +0100

    begin cd

--- LISA_LOCAL/pre_master_main_local_34/cd.c
+++ LISA_LOCAL/pre_master_main_local_34/cd.c
@@ -6,7 +6,7 @@
 /*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/03 13:12:48 by livlamin      #+#    #+#                 */
-/*   Updated: 2021/02/26 13:34:00 by livlamin      ########   odam.nl         */
+/*   Updated: 2021/02/26 14:16:49 by livlamin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,29 @@ void        *alter_env(char **env, char *var, char *file)
     return (NULL);
 }
 
-void        cd(t_command *command, char **env, char *path, int start)
+static int		move_forward(char **env, char *path, char *file)
 {
 	struct stat		buffer;
+	// char	*file;
+
+	// file = NULL;
+	// file = ft_substr(command->args->content, start, len);
+	alter_env(&(*env), "PWD=", file);
+	path = ft_strjoin(path, "/");
+	path = ft_strjoin(path, file);
+	if (stat(path, &buffer) == -1)
+	{
+		printf("minishell: %s: Not a directory\n", file);
+		free(file);
+		return (-1);
+	}
+	free(file);
+	file = NULL;
+	return (0);
+}
+
+void        cd(t_command *command, char **env, char *path, int start)
+{
 	int				len;
 	char			*file;
 
@@ -53,7 +73,7 @@ void        cd(t_command *command, char **env, char *path, int start)
 	file = NULL;
 	path = getcwd(NULL, 0);
     if (path == NULL)
-        error_handler("getcwd failure", NULL, command); //veranderen met geheugen?
+        error_handler("getcwd failure", NULL, command);
 	if (command->args == NULL)
 	{
 		path = check_env(env, "HOME=");
@@ -67,23 +87,17 @@ void        cd(t_command *command, char **env, char *path, int start)
 	{
 		start += len;
 		len = 0;
-		while (command->args->content[start + len] != '.' && command->args->content[start + len] != '/' && command->args->content[start + len] != '\0')
+		// while (command->args->content[start + len] != '.' && command->args->content[start + len] != '/' && command->args->content[start + len] != '\0')
+		// 	len++;
+		while (!ft_strchr("'.'/''\0'", command->args->content[start + len]))
 			len++;
 		if (len > 0)
 		{
 			file = ft_substr(command->args->content, start, len);
-			alter_env(&(*env), "PWD=", file);
-			path = ft_strjoin(path, "/");
-			path = ft_strjoin(path, file);
-			if (stat(path, &buffer) == -1)
-			{
-				printf("minishell: %s: Not a directory\n", file);
-				free(file);
+			if (move_forward(env, path, file) == -1)
 				return;
-			}
-			free(file);
-			file = NULL;
 		}
+			
 		if (command->args->content[start + len] == '.' && command->args->content[start + len + 1] == '.')
 		{
 			path = move_backwards(path);
