e05fbd02ba398ecf18e1e4517feb7b207a4476e9
lisa vlamings <>
2021-03-01 10:48:46 +0100

    35

---
+++ PREMASTER/pre_master_main_16/check_type.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check_type.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/28 17:53:20 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/03/01 10:40:12 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void 	check_type_two(char **env, t_command *command, pid_t process)
+{
+	t_command	*cur_struct;
+
+	cur_struct = command;
+	if (ft_strncmp((const char *)cur_struct->program, "echo", 4) == 0)
+		echo(command);
+	else if (ft_strncmp((const char *)cur_struct->program, "cd", 2) == 0)
+		cd(command, env);
+	else if (ft_strncmp((const char *)cur_struct->program, "pwd", 3) == 0)
+		pwd(command);
+	else if (ft_strncmp((const char *)cur_struct->program, "export", 6) == 0)
+		printf("export type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "unset", 5) == 0)
+		printf("unset type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "env", 3) == 0)
+		printf("env type\n");
+	else if (ft_strncmp((const char *)cur_struct->program, "exit", 4) == 0)
+		exit_func(command);
+	else
+	{
+		// dup_func(command);
+		external(command, env, process);
+	}
+}
+
+void	check_type(char **env, t_command *command)
+{
+	t_command	*cur_struct;
+	pid_t		process;
+
+	cur_struct = command;
+	process = -1;
+	if (redirection(cur_struct) == -1)
+		return;
+	if (command->pipe_right == 1 || command->pipe_left == 1)
+		process = pipes(env, cur_struct);
+	else
+		check_type_two(env, command, process);
+}
