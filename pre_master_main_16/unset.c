57579022b8e40c2a06fc4aa82cad93431b563558
Tixieej <rixtdieuwke@live.nl>
2021-03-09 10:38:54 +0100

    only 3 leaks left... where are they???

--- pre_master_main_16/unset.c
+++ pre_master_main_16/unset.c
@@ -6,7 +6,7 @@
 /*   By: rixt <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/25 14:05:53 by rixt          #+#    #+#                 */
-/*   Updated: 2021/03/08 18:34:17 by rde-vrie      ########   odam.nl         */
+/*   Updated: 2021/03/09 10:36:10 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,15 @@ void	unset(t_command *command, char ***env)
 	if (!(command->args))
 		return ;
 	var_name = command->args->content;
-	//printf("\targ = [%s], var = [%s]\n", command->args->content, var_name);
-	env_var = check_env(*env, var_name);//get the right env variable
-	//printf("\tenv_var = [%s]\n", env_var);
-	// check of var in env lijst staat, zonee, ga uit de functie.
+	env_var = check_env(*env, var_name);
 	if (!env_var)
 	{
 		printf("\tkomt niet voor in env\n");
 		return ;
 	}
 	count = array_length(*env);
-	new_env = malloc(sizeof(char *) * (count));//null aan einde
+	printf("\t COUNT = %i\n", count);
+	new_env = malloc(sizeof(char *) * (count));
 	if (!new_env)//free hier ook dingen?
 		printf("ERROR\n");
 	printf("\tnew_env = %p\n", new_env);
@@ -54,35 +52,31 @@ void	unset(t_command *command, char ***env)
 	//while(1);//hierboven zitten geen leaks meer
 	while ((*env)[i+j])
 	{
-		//printf("env %i = %s\n", i+j, (*env)[i + j]);
-		//2 while loops maken en tussendoor een keer env plussen
-		if (ft_strncmp(var_name, (*env)[i + j], len_var) != 0)//als het niet gelijk is
+		printf("env %i\t(*env)[i+j] = \t%p = %s\n", i+j, (*env)[i+j], (*env)[i+j]);
+		if (ft_strncmp(var_name, (*env)[i + j], len_var) != 0)
 		{
-			//if (j != 0)
-			free(new_env[i]);
-			new_env[i] = ft_strdup((*env)[i + j]);//
+			new_env[i] = ft_strdup((*env)[i + j]);
 		//	printf("\tnew_env = %p, new_env[i] = %p\n", new_env, new_env[i]);
+		//	printf("\t(*env)[i+j] = \t%p\n", (*env)[i+j]);
 			i++;
 		}
 		else
 			j++;
 	}
+	//while(1);//2 leaks in while loop hierboven
+	free_array(*env);
 	*env = new_env;//waar gaat de vorige *env heen?
 	printf("\tnew_env = \t%p\n", new_env);// het is iets wat vlak hiervoor is gemalloct?
-	printf("\tunset env = \t%p (wil je hetzelfde)\n", *env);
-	printf("\tenv = \t%p (hoort anders)\n", env);
+	printf("\tunset env = \t%p\n", *env);
+	printf("\tenv = \t\t%p\n", env);
+
+/*
 	i = 0;
 	//dit zijn checks, mogen weg als het klopt.
 	while (new_env[i])
 	{
 		
-		//printf("\t\tnew_env %i = %s\n", i, new_env[i]);
+		printf("\t\tnew_env %i = %s\n", i, new_env[i]);
 		i++;
-	}
-	i = 0;
-	while ((*env)[i])
-	{
-		//printf("\t\tenv %i = %s\n", i, (*env)[i]);
-		i++;
-	}
+	}*/
 }
