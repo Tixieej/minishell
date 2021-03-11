0f9cdab1dd37d9541d078130532235119f87e7d3
lisa vlamings <>
2021-03-11 14:27:08 +0100

    52 en rixt rauw

---
+++ LISA_CLEANUP/rixt rauw/external.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   external.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rdvrie <marvin@codam.nl>                     +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/01 10:25:42 by rixt          #+#    #+#                 */
+/*   Updated: 2021/03/09 18:52:06 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_array(char **array)
+{
+	int i;
+
+	i = 0;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+void	ft_exec(char *path, t_command cmd, char **env, pid_t process)
+{
+	char	**args;
+	t_list	*arglist;
+	t_list	*new_elem;
+	
+	arglist = cmd.args;
+	new_elem = ft_create_elem(path);
+	if (!new_elem) //check of malloc gelukt is
+		error_handler("malloc failed", NULL, &cmd);
+	ft_lstadd_front(&arglist, new_elem);//list malloct dingen
+	args = list_to_array(&arglist);//args is gemalloct
+//	printf("\tthe process id is now [%d]\n", process);
+	if (process == -1)
+		process = fork();
+	if (process == 0)
+	{
+		if (execve(path, args, env) == -1)
+		{
+			printf("\thoi execve mislukt\n");
+			free_array(args);
+			free(new_elem);
+			// error_handler("argument", NULL, cmd); <- invullen
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(NULL);
+		free(new_elem);
+		//if (args != NULL)
+		//	free_array(args);
+		// sluit fd's af.
+	}
+}
+
+static void	with_path(t_command cmd, char **env, pid_t process)
+{
+	struct stat		buffer;
+	char			*path;
+
+	path = cmd.program;
+	if (stat(path, &buffer) == 0)
+		ft_exec(path, cmd, env, process);
+	else
+		printf("no such file or directory: %s\n", cmd.program);
+}
+
+static void	attach_path(t_command cmd, char **env, pid_t process)
+{
+	char		**paths;
+	char		*path;
+	char		*semi_path;
+	struct stat	buffer;
+	int			i;
+
+	paths = make_path_array(env);
+	i = 0;
+	if (paths == NULL) // dit kan mooier. misschien naar de errorfunctie sluizen?
+	{
+		printf("%s: command not found\n", cmd.program);
+		return ;
+	}
+	while (paths[i])
+	{
+		semi_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(semi_path, cmd.program);
+		free(semi_path);
+		//cmd.args[0] = path;
+		if (stat(path, &buffer) == 0)
+		{
+			free_array(paths);
+			ft_exec(path, cmd, env, process);
+			free(path);
+		//	if (paths != NULL)
+		//		free_array(paths);
+			return ;
+		}
+		free(path);
+		i++;
+	}
+	if (stat(path, &buffer) != 0)
+		printf("%s: command not found\n", cmd.program);
+	free_array(paths);
+}
+
+void	external(t_command *cmd, char **env, pid_t process)
+{
+	int		stdout_fd;
+	int		stdin_fd;
+
+	stdout_fd = out_redirect(cmd);
+	stdin_fd = in_redirect(cmd);
+	if (ft_strchr(cmd->program, '/') != 0)
+		with_path(*cmd, env, process);
+	else
+		attach_path(*cmd, env, process);
+	//while(1); leaks zitten hiervoor	
+	if (cmd->out_red)// dit kan niet fd_out != 1 zijn
+	{
+		if (dup2(stdout_fd, STDOUT_FILENO) < 0)
+		{
+			printf("Unable to duplicate file descriptor.");
+			//exit(EXIT_FAILURE); exiten stopt heel minishell, dus hier komt iets anders
+		}
+		close(cmd->fd_out);
+	}
+	if (cmd->in_red)
+	{
+		if (dup2(stdin_fd, STDIN_FILENO) < 0)
+		{
+			printf("Unable to duplicate file descriptor.");
+		//	exit(EXIT_FAILURE);
+		}
+		close(cmd->fd_in);
+	}
+}
---
+++ LISA_CLEANUP/rixt rauw/pipes.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pipes.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rde-vrie <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/03 14:20:04 by rixt          #+#    #+#                 */
+/*   Updated: 2021/03/04 14:28:07 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	*ft_create_fd_array(t_command *cmd, int times, int *fd_array)
+{
+	int			count;
+
+	count = 0;
+	fd_array = malloc(sizeof(int) * (times * 2) + 1);
+	fd_array[times * 2] = -1;
+	if (!fd_array)
+		error_handler("malloc failed in pipes", NULL, cmd);
+	while (count < times)
+	{
+		if (pipe(fd_array + (count * 2)) < 0)
+		{
+			free(fd_array);
+			error_handler("pipe failed", NULL, cmd);	
+		}
+		count++;
+	}
+	return (fd_array);
+}
+
+static void close_fd_array(int *fd_array)
+{
+	int count;
+
+	count = 0;
+	while (fd_array[count] != -1)
+	{
+		close(fd_array[count]);
+		count++;
+	}
+}
+
+static int count_pipes(t_command *cmd)
+{
+	int times;
+	t_command	*cur_struct;
+
+	cur_struct = cmd;
+	times = 1;
+	while (cur_struct)
+	{
+		if (cur_struct->pipe_right == 1)
+			times++;
+		cur_struct = cur_struct->next;
+	}
+	cur_struct = cmd;
+	return (times);
+}
+
+pid_t	pipes(char ***env, t_command *cmd)
+{
+	t_command	*cur_cmd;
+	int			times;
+	int			*fd_array;
+	pid_t		process;
+	int			count;
+
+	cur_cmd = cmd;
+	count = 0;
+	process = 0;
+	fd_array = NULL;
+	times = count_pipes(cur_cmd);
+	fd_array = ft_create_fd_array(cur_cmd, times, fd_array);
+	while (cur_cmd)
+	{	
+		if ((process = fork()) == 0)
+		{
+			if (cur_cmd->pipe_right == 1 && cur_cmd->pipe_left != 1)
+			{
+				// replace cat's stdout with write part of 1st pipe
+				dup2(fd_array[count + 1], STDOUT_FILENO); //fd[1]
+				close_fd_array(fd_array);
+			}
+			else if (cur_cmd->pipe_right == 1 && cur_cmd->pipe_left == 1)
+			{
+				// replace grep's stdin with read end of 1st pipe
+				dup2(fd_array[count - 2], STDIN_FILENO); //fd[0]
+		 		// replace grep's stdout with write end of 2nd pipe
+				dup2(fd_array[count + 1], STDOUT_FILENO); //fd[3]
+				close_fd_array(fd_array);
+			}
+			else if (cur_cmd->pipe_left == 1 && cur_cmd->pipe_right != 1)
+			{
+				// replace cut's stdin with input read of 2nd pipe
+				dup2(fd_array[count - 2], STDIN_FILENO); //fd[2]
+				close_fd_array(fd_array);
+			}
+			check_type_two(env, cur_cmd, process);
+			exit(0); //belangrijk voor non buildins aangezien er geen process wordt aangemaakt moet die wel gestops worden
+		}
+		if (process == -1)
+			printf("error");
+		cur_cmd = cur_cmd->next;
+		count += 2;
+	}
+	close_fd_array(fd_array);
+	cur_cmd = cmd;
+	free(fd_array);
+	fd_array = NULL;
+	while (wait(NULL) > 0)
+		;
+	return (process);
+}
