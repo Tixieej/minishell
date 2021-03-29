28377da587364471eaf66f0d4ebf44e26966af03
lisa vlamings <>
2021-03-29 14:48:43 +0200

    24 _L is norm

---
+++ PREMASTER/pre_master_main_24_L/dup_redirection.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   dup_redirection.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rixt <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/11 12:01:23 by rixt          #+#    #+#                 */
+/*   Updated: 2021/03/16 15:01:30 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	out_redirect(t_command *cmd)
+{
+	int		stdout_fd;
+
+	stdout_fd = dup(STDOUT_FILENO);
+	if (cmd->fd_out != 1)
+	{
+		if (stdout_fd < 0 || dup2(cmd->fd_out, STDOUT_FILENO) < 0)
+		{
+			command_not_found(cmd, cmd->program, "Bad file descriptor", 9);
+			return (-1);
+		}
+	}
+	return (stdout_fd);
+}
+
+int	in_redirect(t_command *cmd)
+{
+	int		stdin_fd;
+
+	stdin_fd = dup(STDIN_FILENO);
+	if (cmd->fd_in != 0)
+	{
+		if (stdin_fd < 0 || dup2(cmd->fd_in, STDIN_FILENO) < 0)
+		{
+			command_not_found(cmd, cmd->program, "Bad file descriptor", 9);
+			return (-1);
+		}
+	}
+	return (stdin_fd);
+}
