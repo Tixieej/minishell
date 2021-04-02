8bdb0d074e35933d755483cada382fad470ef95f
Tixieej <rixtdieuwke@live.nl>
2021-04-02 15:34:31 +0200

    new version 27 from test

---
+++ pre_master_main_27_L/divide_input.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   divide_input.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/12 10:25:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/04/02 10:20:31 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*trim_quotation_marks(char *temp, char *type, int len, int i)
+{
+	char	*temp_tr;
+
+	temp_tr = NULL;
+	len = ft_strlen(temp);
+	while (temp[i] != '\0')
+	{
+		if (temp[i] == '\"' || temp[i] == '\'')
+		{
+			if (i > 1)
+				type = ft_substr(temp, 0, i);
+			temp_tr = ft_substr(temp, i + 1, len - (i + 1));
+			free(temp);
+			if (type)
+				temp = ft_strjoin(type, temp_tr);
+			else
+				temp = ft_strdup(temp_tr);
+			free(temp_tr);
+			free(type);
+			return (temp);
+		}
+		i++;
+	}
+	return (temp);
+}
+
+static int	create_list_item(t_list **list, char *line,
+								size_t *len, unsigned int start)
+{
+	char	*temp;
+
+	temp = ft_substr((char const *)line, start, *len);
+	temp = trim_quotation_marks(temp, NULL, 0, 0);
+	ft_list_push_back(list, temp);
+	// if (line[start + *len] == ';')
+	// 	ft_list_push_back(list, ft_strdup(";"));
+	if (line[start + *len] != '\0')
+		start += *len;
+	else
+		start += *len - 1;
+	*len = 0;
+	return (start);
+}
+
+static int	handle_quotation_marks(t_list **list, char *line,
+									size_t *len, unsigned int start)
+{
+	if (line[start + *len] == '\'')
+	{
+		(*len)++;
+		while (line[start + *len] != '\'' && line[start + *len] != '\0')
+			(*len)++;
+		if (line[start + *len] != '\'')
+			error_handler("missing quotation marks\n", *list, NULL, 1);
+		start = create_list_item(list, line, len, start);
+	}
+	else if (line[start + *len] == '\"')
+	{
+		(*len)++;
+		while (line[start + *len] != '\"' && line[start + *len] != '\0')
+			(*len)++;
+		if (line[start + *len] != '\"')
+			error_handler("missing quotation marks\n", *list, NULL, 1);
+		start = create_list_item(list, line, len, start);
+	}
+	start += *len;
+	*len = 0;
+	return (start);
+}
+
+static void	prep_redirection(char *line, unsigned int *start, size_t *len)
+{
+	if (line[*start + *len] == '>' && line[*start + *len + 1] == '>')
+	{
+		(*start)++;
+		*len = 2;
+	}
+	if (line[*start + *len] == '>' || line[*start + *len] == '<')
+		(*start)++;
+}
+
+void	divide_input(t_list **list, char *line,
+							size_t len, unsigned int start)
+{
+	while (line[start + len] != '\0')
+	{
+		while (line[start + len] == ' ')
+			start++;
+		if (!ft_strchr("' ''<''>'\'\"'\0'';'", line[start + len]))
+		{
+			start += len;
+			while (!ft_strchr("' '';''<''>' \'\"'\0'", line[start + len]))
+				len++;
+		}
+		if (!ft_strncmp(&line[start + len], ">>>", 3))
+			error_handler("error near unexpected token`>'\n", *list, NULL, 258);
+		if (ft_strchr("\'\"", line[start + len]) && line[start + len] != '\0')
+			start = handle_quotation_marks(list, line, &len, start);
+		if (ft_strchr("';'' ''<''>''\0'", line[start + len]) && len > 0)
+		{
+			if (line[start + len] == '>' || line[start + len] == '<')
+				prep_redirection(line, &start, &len);
+			if (len != 0)
+				start = create_list_item(list, line, &len, start);
+		}
+		len++;
+	}
+}
---
+++ pre_master_main_27_L/error_handler.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   error_handler.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/25 11:04:42 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/04/02 13:51:12 by rixt          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	error_handler(char *error, t_list *list, t_command *command, int exit_status)
+{
+	write(2, "minishell: ", 11); //
+	write(2, error, ft_strlen(error)); //
+	if (list)
+	{
+		ft_lstclear(&list, free);
+		list = NULL;
+	}
+	if (command)
+		command = ft_clear_linked_struct(command);
+	exit(exit_status);
+}
+
+void	command_not_found(t_command *command,
+			char *message, char *error, int value)
+{
+	command->not_found = value;
+	write(2, "minishell: ", 11);
+	write(2, message, ft_strlen(message));
+	write(2, ": ", 2);
+	write(2, error, ft_strlen(error));
+	write(2, "\n", 1);
+}
