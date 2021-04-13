fedbdc35293bde00ba4746c66be7cb216c0cf460
Tixieej <rixtdieuwke@live.nl>
2021-04-13 09:59:58 +0200

    new version

---
+++ pre_master_main_31_L/syntax.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   syntax.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rde-vrie <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/12 14:21:36 by rde-vrie      #+#    #+#                 */
+/*   Updated: 2021/04/12 18:26:31 by rde-vrie      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	syntax_error(t_list *list)
+{
+//	t_list *cur_list;
+	
+//	cur_list = list;
+	// if (ft_strchr(";", cur_list))//als cur list = ;<>| en dan ;<>|  ??
+	// {
+	// 	if (cur_list->next)
+	// 	{
+	// 		write(2, "minishell: syntax error near unexpected token `", 47);
+	// 		write(2, cur_list, ft_strlen(cur_list));
+	// 		write(2, "'\n", 2);
+	// 	}
+	// }
+}
