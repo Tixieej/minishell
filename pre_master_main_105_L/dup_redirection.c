76d131ef87f2480ffb0a3ef0172665400578d60f
Lisa Vlamings <livlamin@f0r4s6.codam.nl>
2021-04-19 17:51:31 +0200

    laatste versie lisa

---
+++ pre_master_main_105_L/dup_redirection.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   dup_redirection.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rixt <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/11 12:01:23 by rixt          #+#    #+#                 */
+/*   Updated: 2021/04/19 15:08:34 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	out_redirect(t_command *cmd)
+{
+	int		stdout_fd;
+
+	stdout_fd = STDOUT_FILENO;
+	if (cmd->fd_out != 1)
+	{
+		stdout_fd = dup(STDOUT_FILENO);
+		if (stdout_fd < 0 || dup2(cmd->fd_out, STDOUT_FILENO) < 0)
+		{
+			close(cmd->fd_out);
+			command_not_found(cmd, cmd->program, "Bad file descriptor", 9);
+			return (-1);
+		}
+		close(cmd->fd_out);
+	}
+	return (stdout_fd);
+}
+
+int	in_redirect(t_command *cmd)
+{
+	int		stdin_fd;
+
+	stdin_fd = STDIN_FILENO;
+	if (cmd->fd_in != 0)
+	{
+		stdin_fd = dup(STDIN_FILENO);
+		if (stdin_fd < 0 || dup2(cmd->fd_in, STDIN_FILENO) < 0)
+		{
+			close(cmd->fd_in);
+			command_not_found(cmd, cmd->program, "Bad file descriptor", 9);
+			return (-1);
+		}
+		close(cmd->fd_in);
+	}
+	return (stdin_fd);
+}
---
+++ pre_master_main_105_L/redirection.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   redirection.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/03 13:16:00 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/04/19 16:00:45 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	in_red(t_command *command, t_list *begin)
+{
+	t_command	**cur_struct;
+
+	begin = command->in_red;
+	cur_struct = &command;
+	while ((*cur_struct)->in_red)
+	{
+		if (!(ft_strncmp((const char *)(*cur_struct)->in_red->content, "<", 1)))
+		{
+			(*cur_struct)->in_red = (*cur_struct)->in_red->next;
+			if ((*cur_struct)->in_red)
+				command->fd_in = open((const char *)
+						(*cur_struct)->in_red->content, O_RDONLY, 0644);
+		}
+		if (command->fd_in < 0)
+		{
+			command_not_found(command, (char *)(*cur_struct)->in_red->content,
+				"No such file or directory", 1);
+			return (-1);
+		}
+		if ((*cur_struct)->in_red->next)
+			close((int)(*cur_struct)->in_red);
+		(*cur_struct)->in_red = (*cur_struct)->in_red->next;
+	}
+	command->in_red = begin;
+	return (0);
+}
+
+void	set_fd(t_command **cur_struct, t_command *command)
+{
+	if (!(ft_strncmp((const char *)
+				(*cur_struct)->out_red->content, ">>", 2)))
+	{
+		(*cur_struct)->out_red = (*cur_struct)->out_red->next;
+		if ((*cur_struct)->out_red)
+			command->fd_out = open((const char *)
+					(*cur_struct)->out_red->content,
+					O_CREAT | O_WRONLY | O_APPEND, 0644);
+	}
+	else if (!(ft_strncmp((const char *)
+				(*cur_struct)->out_red->content, ">", 1)))
+	{
+		(*cur_struct)->out_red = (*cur_struct)->out_red->next;
+		if ((*cur_struct)->out_red)
+			command->fd_out = open((const char *)
+					(*cur_struct)->out_red->content,
+					O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	}
+}
+
+int	out_red(t_command *command)
+{
+	t_command	**cur_struct;
+	t_list		*begin;
+
+	begin = command->out_red;
+	cur_struct = &command;
+	while ((*cur_struct)->out_red)
+	{
+		set_fd(cur_struct, command);
+		if (command->fd_out < 0)
+		{
+			command_not_found(command, (char *)(*cur_struct)->out_red->content,
+				"No such file or directory", 1);
+			return (-1);
+		}
+		if ((*cur_struct)->out_red->next)
+			close((int)(*cur_struct)->out_red);
+		(*cur_struct)->out_red = (*cur_struct)->out_red->next;
+	}
+	command->out_red = begin;
+	return (0);
+}
+
+int	redirection(t_command *command)
+{
+	t_command	*cur_struct;
+
+	cur_struct = command;
+	command->fd_in = STDIN_FILENO;
+	command->fd_out = STDOUT_FILENO;
+	while (cur_struct)
+	{
+		if (in_red(cur_struct, NULL) == -1)
+			return (-1);
+		if (out_red(cur_struct) == -1)
+			return (-1);
+		cur_struct = cur_struct->next;
+	}
+	return (0);
+}
