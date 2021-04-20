c651485e848c6c1012965f8bc542f451172759b6
Tixieej <rixtdieuwke@live.nl>
2021-04-20 17:22:42 +0200

    malloc protection, couldnt fit in function that calls ft_strdup

--- minishell/minishell_01/libft/ft_strdup.c
+++ minishell/minishell_01/libft/ft_strdup.c
@@ -6,11 +6,12 @@
 /*   By: livlamin <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/06 09:01:25 by livlamin      #+#    #+#                 */
-/*   Updated: 2021/03/08 14:25:27 by livlamin      ########   odam.nl         */
+/*   Updated: 2021/04/20 17:11:46 by rixt          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
 char	*ft_strdup(const char *s1)
 {
@@ -20,7 +21,10 @@ char	*ft_strdup(const char *s1)
 	len = ft_strlen(s1);
 	space = (char *)malloc(sizeof(char) * len + 1);
 	if (!space)
-		return (NULL);
+	{
+		write(2, "minishell: malloc failed\n", 25);
+		exit(1);
+	}
 	space[len] = '\0';
 	len = 0;
 	while (s1[len] != '\0')
