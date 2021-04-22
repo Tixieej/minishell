0e6ec19ab93f27974156048359eb571dd0a66712
lisa vlamings <>
2021-04-22 13:08:31 +0200

    03

--- FINALE/minishell_local_02_L/srcs/divide_input.c
+++ FINALE/minishell_local_02_L/srcs/divide_input.c
@@ -6,7 +6,7 @@
 /*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/12 10:25:42 by livlamin      #+#    #+#                 */
-/*   Updated: 2021/04/22 09:01:12 by livlamin      ########   odam.nl         */
+/*   Updated: 2021/04/22 13:06:17 by livlamin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,99 +47,72 @@
 // 	return (str);
 // }
 
-static char	*expansion(t_base *base, char *temp, int *start, size_t *len)
-{
-	int		count;
-	char	*str_start;
-	char	*str;
+// static char	*expansion(t_base *base, char *temp, int *start, size_t *len)
+// {
+// 	int		count;
+// 	char	*str_start;
+// 	char	*str;
 
-	str_start = ft_strdup("");
-	count = 0;
-	str = NULL;
-	if (*start > 0)
-		str_start = ft_substr(temp, 0, *start);
-	while (base->env[count])
-	{
-		if (ft_strncmp(base->env[count], &temp[*start], *len) == 0
-				&& base->env[count][*len] == '=')
-		{
-			temp = ft_strdup(&base->env[count][*len + 1]);
-			count = 0;
-			break ;
-		}
-		count++;
-	}
-	printf("str start %s\n", str_start);
-	printf("str temp %s\n", temp);
-	str = ft_strjoin(str_start, temp);
-	free(temp);
-	free(str_start);
-	// *start += ft_strlen(temp);
-	*len = 0;
+// 	str_start = ft_strdup("");
+// 	count = 0;
+// 	str = NULL;
+// 	if (*start > 0)
+// 		str_start = ft_substr(temp, 0, *start);
+// 	while (base->env[count])
+// 	{
+// 		if (ft_strncmp(base->env[count], &temp[*start], *len) == 0
+// 				&& base->env[count][*len] == '=')
+// 		{
+// 			temp = ft_strdup(&base->env[count][*len + 1]);
+// 			count = 0;
+// 			break ;
+// 		}
+// 		count++;
+// 	}
+// 	printf("str start %s\n", str_start);
+// 	printf("str temp %s\n", temp);
+// 	str = ft_strjoin(str_start, temp);
+// 	free(temp);
+// 	free(str_start);
+// 	// *start += ft_strlen(temp);
+// 	*len = 0;
 
-	return (str);
-}
+// 	return (str);
+// }
 
-static char	*check_expansion(t_base *base, char *temp, size_t len)
-{
-	int		start;
-	char	*str_end;
-	size_t	str_len;
-	char	*str;
+// static char	*check_expansion(t_base *base, char *temp, size_t len)
+// {
+// 	int		start;
+// 	char	*str_end;
+// 	size_t	str_len;
+// 	char	*str;
 
-	str = NULL;	
-	str_len = ft_strlen(temp);
-	start = 0;
-	str_end = ft_strdup("");
-	while (temp[start + len] != '\0')
-	{
-		// if (temp[len] == '\"')
-		// 	len = trim_and_exp(temp, len);
-		// if (temp[len] == '\'')
-		// 	len = trim_quotation_marks(temp, len);
-		if (temp[start + len] == '$')
-		{
-			start++;
-			while (temp[start + len] != '\0' && temp[start + len] != '$' && temp[start + len] != '\'' && temp[start + len] != '\"')
-				len++;
-			if (str_len > len)
-				str_end = ft_strdup(&temp[start + len]);
-			str = ft_strjoin(expansion(base, temp, &start, &len), str_end);
-			free(temp);
-			temp = ft_strdup(str);
-			free(str);
-		}
-		len++;
-	}
-	return (temp);
-}
-
-static void	check_missing_q(char *temp, int	check_solo, int check_double)
-{
-	int len;
-
-	len = 0;
-	while (temp[len] != '\0')
-	{
-		if (temp[len] == '\'')
-		{
-			while (temp[len] != '\0')
-			{
-				if (temp[len] == '\'');
-				check_solo++;
-			}
-
-		}
-			
-			
-		if (temp[len] == '\"')
-			check_double++;
-		len++;
-	}
-	if ((check_solo & 1) == 1 || (check_double & 1) == 1)
-		error_handler("missing quotation marks\n", NULL, NULL, 2);
-}
-
+// 	str = NULL;	
+// 	str_len = ft_strlen(temp);
+// 	start = 0;
+// 	str_end = ft_strdup("");
+// 	while (temp[start + len] != '\0')
+// 	{
+// 		// if (temp[len] == '\"')
+// 		// 	len = trim_and_exp(temp, len);
+// 		// if (temp[len] == '\'')
+// 		// 	len = trim_quotation_marks(temp, len);
+// 		if (temp[start + len] == '$')
+// 		{
+// 			start++;
+// 			while (temp[start + len] != '\0' && temp[start + len] != '$' && temp[start + len] != '\'' && temp[start + len] != '\"')
+// 				len++;
+// 			if (str_len > len)
+// 				str_end = ft_strdup(&temp[start + len]);
+// 			str = ft_strjoin(expansion(base, temp, &start, &len), str_end);
+// 			free(temp);
+// 			temp = ft_strdup(str);
+// 			free(str);
+// 		}
+// 		len++;
+// 	}
+// 	return (temp);
+// }
 static int	create_list_item(t_base *base, char *line,
 								size_t *len, unsigned int start)
 {
@@ -149,9 +122,8 @@ static int	create_list_item(t_base *base, char *line,
 	temp = NULL;
 	check = 0;
 	temp = ft_substr((char const *)line, start, *len);
-	check_missing_q(temp, 0, 0);
-	temp = check_expansion(base, temp, 0);
-	// printf("temp: %s\n", temp);
+	// check_missing_q(temp, 0);
+	// temp = check_expansion(base, temp, 0);
 	if (temp)
 		ft_list_push_back(&base->list, temp);
 	if (line[start + *len] != '\0')
@@ -162,13 +134,45 @@ static int	create_list_item(t_base *base, char *line,
 	return (start);
 }
 
+static void	check_missing_q(char *temp, unsigned int *start, size_t *len)
+{
+	while (temp[*start + *len] != '\0')
+	{
+		if (temp[*start + *len] == '\'')
+		{
+			(*len)++;
+			while (temp[*start + *len] != '\'' && temp[*start + *len] != '\0')
+				(*len)++;
+			if (temp[*start + *len] != '\'')
+				error_handler("missing quotation marks\n", NULL, NULL, 2);
+			(*len)++;
+			if (temp[*start + *len] == ' ' || temp[*start + *len] == '\0')
+				break;
+		}
+		else if (temp[*start + *len] == '\"')
+		{
+			(*len)++;
+			while (temp[*start + *len] != '\"' && temp[*start + *len] != '\0')
+				(*len)++;
+			if (temp[*start + *len] != '\"')
+				error_handler("missing quotation marks\n", NULL, NULL, 2);
+			(*len)++;
+			if (temp[*start + *len] == ' ' || temp[*start + *len] == '\0')
+				break;
+		}
+		(*len)++;
+	}
+}
+
+
+
 static void	handle_redirection(t_base *base, char *line, size_t *len, unsigned int *start)
 {
 	if (line[*start + *len] == '>' && line[*start + *len + 1] == '>')
-		{
+	{
 			ft_list_push_back(&base->list, ft_strdup(">>"));
 			*start += 1;
-		}
+	}
 	else if (line[*start + *len] == '>' || line[*start + *len] == '<')
 			ft_list_push_back(&base->list, ft_strdup(">"));
 }
@@ -180,15 +184,15 @@ void	divide_input(t_base *base, char *line,
 	{
 		while (line[start + len] == ' ')
 			start++;
-		if (!ft_strchr("'|'';'' ''<''>''\0'", line[start + len])
-			|| line[start + len] =='\'')
+		if (!ft_strchr("'|'';'' ''<''>''\'''\"''\0'", line[start + len]))
 		{
-			while (!ft_strchr("'|'';'' ''<''>''\0'", line[start + len])
-				|| line[start + len] =='\'')
+			while (!ft_strchr("'|'';'' ''\'''\"''<''>''\0'", line[start + len]))
 				len++;		
 		}
 		if (!ft_strncmp(&line[start + len], ">>>", 3))
 			error_handler("error near unexpected token`>'\n", base->list, NULL, 258);
+		if (line[start + len] == '\'' || line[start + len] == '\"')
+			check_missing_q(line, &start, &len);
 		if (ft_strchr("'|''<''>'';'' ''\0'", line[start + len]))
 		{
 			if (len != 0)
